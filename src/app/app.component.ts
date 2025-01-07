40a74b66b21c89d322467c6e21b2ac66de98fcee
Prithiksam J <prithiksam.j@noblq.com>
2025-01-07 10:52:29 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AutoCompleteModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clone-angular';
+  title = 'angular-clone';
+  selectedOption: string = ''; 
+  options = [];
+ items: string[] = []; 
+value: any;
+
+  // Filter method for p-autocomplete
+  search(event: any): void {
+    const query = event.query.toLowerCase();
+    this.items = this.options
+  
+  }
 }
