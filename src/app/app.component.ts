537d16249994ff68de5aa36db27a975ab985285d
Prithiksam J <prithiksam.j@noblq.com>
2025-01-08 10:54:49 +0530

    code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,17 @@ import { PageComponent } from './common/page/page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
+  
   imports: [PageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  title = 'Angular';
+  ngOnInit() {
+    console.log('PageComponet');
+  }
+  ngOnChanges(changes:any){
+    console.log(changes);
+}
 }
--- src/app/common/page/page.component.ts
+++ src/app/common/page/page.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input,OnChanges, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 @Component({
   selector: 'app-page',
-  imports: [FormsModule,AutoCompleteModule,],
+  imports: [FormsModule, AutoCompleteModule],
   templateUrl: './page.component.html',
-  styleUrl: './page.component.css'
+  styleUrl: './page.component.css',
 })
-export class PageComponent {
-    title = 'angular-clone';
-    selectedOption: string = ''; 
-    options = [];
-   items: string[] = []; 
-  value: any;   
-    search(event: any): void {
-      const query = event.query.toLowerCase();
-      this.items = this.options
-    
-    }
+export class PageComponent implements OnInit,OnChanges {
+  title = 'Learn Angular';
+  selectedOption: string = '';
+  options = [];
+  items: string[] = [];
+  value: any;
+  search(event: any): void {
+    const query = event.query.toLowerCase();
+    this.items = this.options;
   }
+  @Input() page: string = '';
 
+  ngOnInit() {
+    console.log('PageComponet');
+  }
+  ngOnChanges(changes:any){
+    console.log(changes);
+  }
+}
